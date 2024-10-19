108f542219853963289a460962add9788c957cb7
Ivan Goriachev <isgoriachev@yandex-team.ru>
2024-10-19 18:55:54 +0300

    made array-working functions instead of macroses

--- idz1/macros.asm
+++ idz1/macros.asm
@@ -2,8 +2,6 @@
 .align 2
 sep: .asciz " "
 
-.global sep
-
 .macro print_sep()
     li a7, 4
     la a0, sep
@@ -22,59 +20,15 @@ sep: .asciz " "
     ecall
 .end_macro
 
-.macro print_array(%array_pointer, %n)
-    la t3, %array_pointer
-    mv t4, %n
-    
-    li t1, 0 # t1 = i
-    
-    read_array_loop:
-        bge t1, t4, read_array_loop_end
-    
-        
-        # calculate a[i] address
-        li t2, 4
-        mul t2, t2, t1 # t2 = t1 * 4
-        add t2, t2, t3 # t2 = array_pointer + (t1 * 4)
-       
-        lw a0, 0(t2) # a0 = a[i]
-        print_int(a0)
-        print_sep()
-        
-        addi t1, t1, 1 # i += 1
-        
-        j read_array_loop
-    read_array_loop_end:
+.macro calculate_ai_address(%i, %array_start_address, %ai_address)
+    li t6, 4
+    mul t6, t6, %i # t6 = i * 4
+    add %ai_address, %array_start_address, %ai_address # a[i]_address = array_pointer + (i * 4)
 .end_macro
-
-.macro read_array_multiple_strings(%array_pointer, %n)
-    la t3, %array_pointer
-    mv t4, %n
-
-    li t1, 0 # t1 = i
-    
-    read_array_loop:
-        bge t1, t4, read_array_loop_end
-        
-        # calculate a[i] address
-        li t2, 4
-        mul t2, t2, t1 # t2 = t1 * 4
-        add t2, t2, t3 # t2 = array_pointer + (t1 * 4)
-        
-        read_int(a0)
-        sw a0, 0(t2) # a[i] = a0 = input
-        addi t1, t1, 1 # i += 1
-        
-        j read_array_loop
-    read_array_loop_end:
-.end_macro
-
-.macro parse_array_from_single_string(%array_pointer, %n, %buffer)
-
-.end_macro
-
 
 .macro exit()
     li a7, 10
     ecall
 .end_macro
+
+
--- idz1/main.asm
+++ idz1/main.asm
@@ -1,21 +1,83 @@
 .include "macros.asm"
+# .include "array_functions.asm"
 .data
+
 .align 2 
 A: .space 64
 B: .space 64
 
+
+
 .text
 main:
+    li a0, 100
+    print_int(a0)
+    
     addi sp, sp, -4
     # 0(sp) = n
 
     read_int(a0)
     sw a0, 0(sp) # 0(sp) = n
-    # lw a0, 0(sp) # unneded operation, a0 is already = 0(sp)
-    read_array_multiple_strings(A, a0)
 
-    lw a0, 0(sp)
-    print_array(A, a0)
+    la a0, A     # a0 = array pointer
+    lw a1, 0(sp) # a1 = n
+    jal ra, read_array
+
+    la a0, A     # a0 = array pointer
+    lw a1, 0(sp) # a1 = n
+    jal ra, print_array
     
     addi sp, sp, 4
     exit()
+    
+
+print_array:
+    # a0 = array_pointer
+    # a1 = n
+    mv t3, a0
+    mv t4, a1
+    
+    li t1, 0 # t1 = i
+    
+    print_array_loop:
+        bge t1, t4, print_array_loop_end
+    
+        
+        # calculate a[i] address
+        li t2, 4
+        mul t2, t2, t1 # t2 = t1 * 4
+        add t2, t2, t3 # t2 = array_pointer + (t1 * 4)
+       
+        lw a0, 0(t2) # a0 = a[i]
+        print_int(a0)
+        print_sep()
+        
+        addi t1, t1, 1 # i += 1
+        
+        j print_array_loop
+    print_array_loop_end:
+    jalr zero, 0(ra)
+    
+read_array:
+    mv t3, a0 # a0 = array_pointer
+    mv t4, a1 # a1 = n
+
+    li t1, 0 # t1 = i
+    
+    read_array_loop:
+        bge t1, t4, read_array_loop_end
+        
+        # calculate a[i] address
+        li t2, 4
+        mul t2, t2, t1 # t2 = t1 * 4
+        add t2, t2, t3 # t2 = array_pointer + (t1 * 4)
+        
+        read_int(a0)
+        sw a0, 0(t2) # a[i] = a0 = input
+        addi t1, t1, 1 # i += 1
+        
+        j read_array_loop
+    read_array_loop_end:
+    jalr zero, 0(ra)
+
+
